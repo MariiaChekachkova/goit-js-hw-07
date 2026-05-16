9d205bbf1d852937316effed4127521b4bfefb92
Mariia Chekachkova <chekachkovam@gmail.com>
2026-05-16 18:48:05 +0200

    first commit

---
+++ js/task-3.js
@@ -0,0 +1,11 @@
+const input = document.querySelector('#name-input');
+const output = document.querySelector('#name-output');
+
+input.addEventListener('input', (event) => {
+    const trimmedValue = event.currentTarget.value.trim();
+    if (trimmedValue === '') {
+        output.textContent = 'Anonymous';
+    } else {
+        output.textContent = trimmedValue;
+    }
+});
